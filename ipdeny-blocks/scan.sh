ed0b72f41041b4a38e337718fdd150171eb67a26
Adrian St. Onge <astonge@gmail.com>
2019-10-12 16:57:04 -0700

    masscan

--- ipdeny-blocks/scan.sh
+++ ipdeny-blocks/scan.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+if [ -z $1 ]
+then
+	echo "need ip block list file"
+	exit 1
+fi
+
+CMDOPT='-p1177 --rate 25'
+
 while read ipblock
 do
-	echo $ipblock
+	sudo /usr/bin/masscan $CMDOPT $ipblock
 	sleep 10
 done < $1
